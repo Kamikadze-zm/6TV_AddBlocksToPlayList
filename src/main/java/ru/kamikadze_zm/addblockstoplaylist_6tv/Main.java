3519c95199626b86a60eb335615e8d881925728f
Pavel Ryabov <Kamikadze.zm@gmail.com>
2018-06-28 00:11:15 +1000

    Добавлена проверка наличия (если нет – создание) папки приложения

--- src/main/java/ru/kamikadze_zm/addblockstoplaylist_6tv/Main.java
+++ src/main/java/ru/kamikadze_zm/addblockstoplaylist_6tv/Main.java
@@ -1,7 +1,22 @@
 package ru.kamikadze_zm.addblockstoplaylist_6tv;
 
+import java.io.File;
+
 public class Main {
     
+    private static final String DEFAULT_APP_NAME = "6TV_AddBlocksToPlayList";
+    
+    public static final String HOME_DIRECTORY = System.getProperty("user.home") + File.separator
+            + "documents" + File.separator + getAppName() + File.separator;
+    
     public static void main(String[] args) {
+        File homeDirectory = new File(HOME_DIRECTORY);
+        if (!homeDirectory.exists()) {
+            homeDirectory.mkdirs();
+        }
+    }
+    
+    private static String getAppName() {
+        return DEFAULT_APP_NAME;
     }
 }
